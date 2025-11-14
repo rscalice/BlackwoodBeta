4a736db88aee8eee45888c20906bc56ba571d991
Robert Scalice <r.scalice@live.com>
2025-11-13 20:01:01 -0500

    Upgrading to 5.7

--- Source/BlackwoodBeta.Target.cs
+++ Source/BlackwoodBeta.Target.cs
@@ -8,7 +8,7 @@ public class BlackwoodBetaTarget : TargetRules
 	public BlackwoodBetaTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "BlackwoodBeta" } );
 	}
