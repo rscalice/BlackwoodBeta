14980c57071ccdfc35931330c7a3c2f5d92a085d
Robert Scalice <r.scalice@live.com>
2026-02-20 21:32:58 -0500

    Adding defense attribute. Reworking damage effects and attribute work

--- Source/BlackwoodBeta/Private/BaseActorAttributes.cpp
+++ Source/BlackwoodBeta/Private/BaseActorAttributes.cpp
@@ -9,14 +9,39 @@ void UBaseActorAttributes::OnRep_Health(const FGameplayAttributeData& OldHealth)
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, Health, OldHealth);
 }
 
+void UBaseActorAttributes::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, MaxHealth, OldMaxHealth);
+}
+
 void UBaseActorAttributes::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, Stamina, OldStamina);
 }
 
+void UBaseActorAttributes::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, MaxStamina, OldMaxStamina);
+}
+
+void UBaseActorAttributes::OnRep_Defense(const FGameplayAttributeData& OldDefense)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, Defense, OldDefense);
+}
+
 void UBaseActorAttributes::OnRep_Experience(const FGameplayAttributeData& OldExperience)
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, Experience, OldExperience);
+}
+
+void UBaseActorAttributes::OnRep_MaxExperience(const FGameplayAttributeData& OldMaxExperience)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, MaxExperience, OldMaxExperience);
+}
+
+void UBaseActorAttributes::OnRep_Damage(const FGameplayAttributeData& OldDamage)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseActorAttributes, Damage, OldDamage);
 }
 
 void UBaseActorAttributes::OnRep_Level(const FGameplayAttributeData& OldLevel)
@@ -32,4 +57,44 @@ void UBaseActorAttributes::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&
 	DOREPLIFETIME_CONDITION_NOTIFY(UBaseActorAttributes, Stamina, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UBaseActorAttributes, Experience, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UBaseActorAttributes, Level, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseActorAttributes, Defense, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseActorAttributes, Damage, COND_None, REPNOTIFY_Always);
+}
+
+void UBaseActorAttributes::PreAttributeChange(const FGameplayAttribute& Attribute, float& delta)
+{
+	Super::PreAttributeChange(Attribute, delta);
+
+	if (Attribute == GetHealthAttribute())
+	{
+	}
+}
+
+void UBaseActorAttributes::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+    if (Data.EvaluatedData.Attribute == GetDamageAttribute())
+    {
+        // 1. Get the damage magnitude
+        float LocalDamageDone = GetDamage();
+        SetDamage(0.f); // 2. Reset meta-attribute
+
+        if (LocalDamageDone > 0.f)
+        {
+			float defense = FMath::Clamp(GetDefense(), 0.f, 500.f);
+			float newDamage = LocalDamageDone * ((1000 - defense) / 1000);
+			//UE_LOGFMT(LogCore, Warning, "Defense: {defense}, DamageDone: {damage} | New Damage {new}",
+			//	defense, LocalDamageDone, newDamage);
+
+            // 3. Apply damage to Health
+            float NewHealth = GetHealth() - newDamage;
+            SetHealth(FMath::Clamp(NewHealth, 0.f, GetMaxHealth()));
+        }
+    }
+    else if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+    {
+        // 4. Handle direct health changes (e.g., healing or direct damage)
+        SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+    }
 }
--- Source/BlackwoodBeta/Public/BaseActorAttributes.h
+++ Source/BlackwoodBeta/Public/BaseActorAttributes.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayEffectExtension.h"
 #include "BaseActorAttributes.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
@@ -29,13 +30,34 @@ public:
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Health);
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, MaxHealth);
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_Stamina)
 	FGameplayAttributeData Stamina;
 	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Stamina);
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_MaxStamina)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, MaxStamina);
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_Experience)
 	FGameplayAttributeData Experience;
 	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Experience);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_MaxExperience)
+	FGameplayAttributeData MaxExperience;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, MaxExperience);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_Defense)
+	FGameplayAttributeData Defense;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Defense);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Base Attributes", ReplicatedUsing = OnRep_Damage)
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UBaseActorAttributes, Damage);
+
 
 	UFUNCTION()
 	virtual void OnRep_Level(const FGameplayAttributeData& OldLevel);
@@ -44,10 +66,30 @@ public:
 	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
 
 	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	UFUNCTION()
 	virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
+
+	UFUNCTION()
+	virtual void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina);
 
 	UFUNCTION()
 	virtual void OnRep_Experience(const FGameplayAttributeData& OldExperience);
 
+	UFUNCTION()
+	virtual void OnRep_MaxExperience(const FGameplayAttributeData& OldMaxExperience);
+
+	UFUNCTION()
+	virtual void OnRep_Defense(const FGameplayAttributeData& OldDefense);
+
+
+	UFUNCTION()
+	virtual void OnRep_Damage(const FGameplayAttributeData& OldDamage);
+
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float & delta);
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data);
 };
