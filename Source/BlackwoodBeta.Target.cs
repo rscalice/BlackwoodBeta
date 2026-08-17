e99446fd31bc155e323f38dba8262239cf450569
Robert Scalice <r.scalice@live.com>
2026-08-16 22:10:06 -0400

    baselining left overs

--- Source/BlackwoodBeta.Target.cs
+++ Source/BlackwoodBeta.Target.cs
@@ -8,7 +8,7 @@ public class BlackwoodBetaTarget : TargetRules
 	public BlackwoodBetaTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 
 		ExtraModuleNames.AddRange( new string[] { "BlackwoodBeta" } );
 	}
